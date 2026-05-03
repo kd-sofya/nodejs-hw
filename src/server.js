754a6ad015b56d81065722de62fca2bdab8ba0d1
Sofia <sofiakucmida@gmail.com>
2026-05-03 17:21:46 +0300

    fix error

--- src/server.js
+++ src/server.js
@@ -1,19 +1,32 @@
 
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
 import dotenv from 'dotenv';
 dotenv.config();
-
-
+import pino from 'pino-http';
 
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(helmet());
+
 app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
