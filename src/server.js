9e15cf4e81f09d2d0ff2600778913fbaef52275c
Sofia <sofiakucmida@gmail.com>
2026-05-03 17:13:28 +0300

    01-express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,52 @@
 
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: "Retrieved all notes",
+  });
+});
+
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
