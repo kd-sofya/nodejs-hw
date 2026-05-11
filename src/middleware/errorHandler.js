ef3807fcbb3cfd4d54854cb9fa037cbde8a41765
Sofia <sofiakucmida@gmail.com>
2026-05-11 19:15:49 +0300

    ///

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,7 +2,7 @@ import { HttpError } from "http-errors";
 
 export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
-    return res.status(err.statusCode || 400).json({
+    return res.status(err.status|| 400).json({
       message: err.message,
     });
   }
